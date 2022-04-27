cb866823c47c7898ca82b9a7b9835bf5ce2d351d
OA <omaba004@osloskolen.no>
2022-04-27 13:30:15 +0200

    finished with the project 🎨🧶🧵🎭🎭🎭

---
+++ README.md
@@ -0,0 +1,2 @@
+### Acknowledgment
+Thanks to this tutorial which I followed along. Here's the turoial: [weatherapp tutorial](https://www.youtube.com/watch?v=WZNG8UomjSI)
--- app.js
+++ app.js
@@ -9,11 +9,32 @@ const weather = {
     },
     displayWeather: function(data) {
     const { name } = data;
-    const { icon, description } = data.weather;
+    const { icon, description, main } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed )
-    }
+    document.querySelector(".city").innerText = `Weather in ${name}`;
+    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector(".description").innerText = `${description}`;
+    document.querySelector(".temp").innerHTML = `<h1 class="temp">${temp}&#176;C</h1>`;
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerText = `Wind speed:${speed} km/h`;
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${name} ")`
+},
+  search: function() {
+    this.fetchWeather(document.querySelector("#input").value);
+  }
 };
 
-const button = document.querySelector("button")
+document.querySelector("button").addEventListener("click", () => {
+ weather.search();
+});
+
+document.querySelector("#input").addEventListener("keyup", (e) => {
+    if(e.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Osaka");
+
